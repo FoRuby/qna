88e5530a8734fd2ecebcebf34dc41c21d454f28c
R1zrei <aggabitov@yandex.ru>
2019-11-18 18:08:31 +0300

    ActiveStorage Question: add files on create

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Question, type: :model do
   context 'associations' do
     it { should have_many(:answers).dependent(:destroy) }
     it { should belong_to(:user) }
+    it 'have many attached files' do
+      expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+    end
   end
 
   context 'validations' do
