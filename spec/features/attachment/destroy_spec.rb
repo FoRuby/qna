e17335ae4155635a11809a0b34cc028d2c39e11b
R1zrei <aggabitov@yandex.ru>
2019-11-20 13:15:26 +0300

    ActiveStorage Answer: delete files

--- spec/features/attachment/destroy_spec.rb
+++ spec/features/attachment/destroy_spec.rb
@@ -6,54 +6,84 @@ feature 'User can destroy question attachment', %q{
   I'd like to be able to destroy question attachment
 } do
 
-  given(:question_author) { create(:user) }
   given(:user) { create(:user) }
+
+  given(:question_author) { create(:user) }
   given(:question) { create(:question, user: question_author) }
-  given!(:attached_file) { question.files.attach(create_file_blob).first }
+  given!(:question_attached_file) { question.files.attach(create_file_blob).first }
+
+  given(:answer_author) { create(:user) }
+  given(:answer) { create(:answer, question: question, user: answer_author) }
+  given!(:answer_attached_file) { answer.files.attach(create_file_blob).first }
 
   describe 'Authenticated', js: true do
     scenario 'question author tries delete a question attachment' do
       login(question_author)
       visit question_path(question)
-      click_on 'Delete attachment'
+      within '.question' do
+        click_on 'Delete attachment'
+        find('.delete-attachment-icon').click
+        accept_confirm
 
-      find("#delete-attachment-icon-#{attached_file.id}").click
-      accept_confirm
-
+        expect(page).to_not have_content question_attached_file.filename.to_s
+        expect(page).to_not have_selector('.delete-attachment-icon')
+      end
       expect(page).to have_content 'Attachment successfully deleted.'
-      expect(page).to_not have_content attached_file.filename.to_s
-      expect(page).to_not have_selector('.delete-attachment-icon')
     end
 
 
-    # scenario 'author tries delete a answer attachment' do
+    scenario 'answer author tries delete a answer attachment' do
+      login(answer_author)
+      visit question_path(question)
 
-    # end
+      within "#answer-#{answer.id}" do
+        click_on 'Delete attachment'
+        find('.delete-attachment-icon').click
+        accept_confirm
+
+        expect(page).to_not have_content answer_attached_file.filename.to_s
+        expect(page).to_not have_selector('.delete-attachment-icon')
+      end
+      expect(page).to have_content 'Attachment successfully deleted.'
+    end
 
     scenario 'user tries delete foreign question attachment' do
       login(user)
       visit question_path(question)
-
-      expect(page).to_not have_link 'Delete attachment'
-      expect(page).to_not have_selector('.delete-attachment-icon')
+      within '.question' do
+        expect(page).to_not have_content 'Delete attachment'
+        expect(page).to_not have_selector('.delete-attachment-icon')
+      end
     end
 
-    # scenario 'user tries delete foreign answer attachment' do
-
-    # end
+    scenario 'user tries delete foreign answer attachment' do
+      login(user)
+      visit question_path(question)
+      within "#answer-#{answer.id}" do
+        expect(page).to_not have_content 'Delete attachment'
+        expect(page).to_not have_selector('.delete-attachment-icon')
+      end
+    end
   end
 
   describe 'Unauthenticated', js: true do
     scenario 'user tries delete a question attachment' do
       visit question_path(question)
 
-      expect(page).to_not have_content 'Delete question'
-      expect(page).to_not have_selector('.delete-attachment-icon')
+      within '.question' do
+        expect(page).to_not have_content 'Delete attachment'
+        expect(page).to_not have_selector('.delete-attachment-icon')
+      end
     end
 
-    # scenario 'user tries delete a answer attachment' do
+    scenario 'user tries delete a answer attachment' do
+      visit question_path(question)
 
-    # end
+      within "#answer-#{answer.id}" do
+        expect(page).to_not have_content 'Delete attachment'
+        expect(page).to_not have_selector('.delete-attachment-icon')
+      end
+    end
   end
 end
 
