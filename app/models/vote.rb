00b1cb0ceaedd6b1b06d22e69e6a94ea744907ed
FoRuby <47187414+FoRuby@users.noreply.github.com>
2020-02-21 21:16:39 +0300

    Caching (#26)

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :user
-  belongs_to :votable, polymorphic: true
+  belongs_to :votable, polymorphic: true, touch: true
 
   validates :votable, presence: true
   validates :value, presence: true,
