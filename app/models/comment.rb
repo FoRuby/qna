54b804157825164444869ea86c83d3c465b8749e
R1zrei <aggabitov@yandex.ru>
2020-02-21 19:53:02 +0300

    Caching

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   default_scope -> { order(created_at: :asc) }
 
   belongs_to :user
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, touch: true
 
   validates  :user, :body, :commentable, presence: true
 end
